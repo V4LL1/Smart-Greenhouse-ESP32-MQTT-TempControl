f228df4b18b8d6abe24c5af72742b8c47675a999
Guilherme Valli <guilherme.palmanhani@gmail.com>
2024-10-20 17:05:27 -0300

    testing components

---
+++ Test_Components/hello_world/hello_world.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // Inicializa a comunicação serial com a velocidade de 115200 baud
+  Serial.begin(9600);
+  Serial.println("Hello, World!");
+}
+
+void loop() {
+  // Não faz nada no loop
+}
+
